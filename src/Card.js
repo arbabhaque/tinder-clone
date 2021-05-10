b3f644a2e6c7ff129ca7e1b03aed76cb2325b68e
Arbab Haque <arbab.haque@gmail.com>
2021-05-10 17:48:43 -0400

    Added Header and Card U.I. Components

---
+++ src/Card.js
@@ -0,0 +1,51 @@
+import "./Card.css";
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import { SwipeableDrawer } from "@material-ui/core";
+
+function Card(){
+    const [people, setPeople] = useState([
+        {
+            name: 'Ash Ketchum',
+            url: 'https://cdn2.bulbagarden.net/upload/thumb/c/cd/Ash_JN.png/150px-Ash_JN.png'
+        },
+        {
+            name: 'Gary Oak',
+            url: 'https://cdn2.bulbagarden.net/upload/thumb/a/ad/Gary_Oak_BW.png/200px-Gary_Oak_BW.png'
+        }
+    ])
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+        //setLastDirection(direction);
+    }
+
+    const outOfFrame = (name) => {
+        console.log(name+" left the screen!")
+    }
+
+    return (
+        <div className="card">
+            <div className="cardsContainer">
+                {people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={['up','down']}
+                        onSwipe={(dir)=>swiped(dir, person.name)}
+                        onCardLeftScreen={()=>outOfFrame(person.name)}
+                    >
+                        <div
+                            style={{backgroundImage: `url(${person.url})`}}
+                            className="profileCard"
+                        >
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Card
