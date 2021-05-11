8278744e067fb74be9c122f92ef4c49f69de0495
Arbab Haque <arbab.haque@gmail.com>
2021-05-11 13:27:08 -0400

    Including parent folder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+import Card from './Card';
+import SwipeButtons from './SwipeButtons';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Card/>
+      <SwipeButtons/>
+    </div>
+  );
+}
+
+export default App;
